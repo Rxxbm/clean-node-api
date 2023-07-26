9569b6deca21b6832ceb98b1e0c058d41fc55144
Rubem Neto <rubemcorrea0@gmail.com>
2023-07-26 17:37:42 -0300

    feat: ensure SignUpController returns 400 if no password_confirmation is provided

--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -27,4 +27,30 @@ describe('SignUp Controller', () => {
         expect(httpResponse.statusCode).toBe(400)
         expect(httpResponse.body).toEqual(new MissingParamsError("email"))
     })
+    test('Should return 400 if no password is provided', () => {
+        const sut = new SignUpController() //system under test
+        const httpRequest = {
+            body:{
+                name: "any_name",
+                email: "any_email",
+                password_confirmation: "any_password"
+            }
+        }
+        const httpResponse = sut.handle(httpRequest)
+        expect(httpResponse.statusCode).toBe(400)
+        expect(httpResponse.body).toEqual(new MissingParamsError("password"))
+    })
+    test('Should return 400 if no password_confirmation is provided', () => {
+        const sut = new SignUpController() //system under test
+        const httpRequest = {
+            body:{
+                name: "any_name",
+                email: "any_email",
+                password: "any_password",
+            }
+        }
+        const httpResponse = sut.handle(httpRequest)
+        expect(httpResponse.statusCode).toBe(400)
+        expect(httpResponse.body).toEqual(new MissingParamsError("password_confirmation"))
+    })
 })
