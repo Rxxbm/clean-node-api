1d8885fe6d0441e72cae2ab2a4443803413d0363
Rubem Neto <rubemcorrea0@gmail.com>
2023-09-27 11:09:15 -0300

    test: ensure SignUpController returns 400 if no password is provided

--- src/presentation/controllers/sign-up/sign-up.spec.ts
+++ src/presentation/controllers/sign-up/sign-up.spec.ts
@@ -25,4 +25,16 @@ describe('SignUp Controller', () => {
     const httpResponse = sut.handle(httpRequest);
     expect(httpResponse.statusCode).toBe(400);
   });
+  test('Should returns 400 if no password is provided', () => {
+    const sut = new SignUpController();
+    const httpRequest = {
+        body: {
+            name: 'any_name',
+            email: 'any_email',
+            password_confirmation: 'any_password'
+        }
+    };
+    const httpResponse = sut.handle(httpRequest);
+    expect(httpResponse.statusCode).toBe(400);
+  });
 });
