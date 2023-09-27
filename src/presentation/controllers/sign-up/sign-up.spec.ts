b3f6d8c9d3c09c88db9a27afc629e53186ce0141
Rubem Neto <rubemcorrea0@gmail.com>
2023-09-27 10:55:29 -0300

    test: ensure SignUpController returns 400 if no email is provided

--- src/presentation/controllers/sign-up/sign-up.spec.ts
+++ src/presentation/controllers/sign-up/sign-up.spec.ts
@@ -13,4 +13,16 @@ describe('SignUp Controller', () => {
     const httpResponse = sut.handle(httpRequest);
     expect(httpResponse.statusCode).toBe(400);
   });
+  test('Should returns 400 if no email is provided', () => {
+    const sut = new SignUpController();
+    const httpRequest = {
+        body: {
+            name: 'any_name',
+            password: 'any_password',
+            password_confirmation: 'any_password'
+        }
+    };
+    const httpResponse = sut.handle(httpRequest);
+    expect(httpResponse.statusCode).toBe(400);
+  });
 });
