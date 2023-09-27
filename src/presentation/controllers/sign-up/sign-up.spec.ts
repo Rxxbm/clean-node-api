6360283b5c5daed51b1ed872077b2dac738cfc14
Rubem Neto <rubemcorrea0@gmail.com>
2023-09-27 20:22:52 -0300

    test: ensure SignUpController returns 400 if password confirmation is invalid

--- src/presentation/controllers/sign-up/sign-up.spec.ts
+++ src/presentation/controllers/sign-up/sign-up.spec.ts
@@ -1,3 +1,4 @@
+import { InvalidParamException } from "../../exceptions/invalid-param-exceptions";
 import { MissingParamException } from "../../exceptions/missing-param-exception";
 import { SignUpController } from "./sign-up";
 
@@ -54,5 +55,18 @@ describe('SignUp Controller', () => {
     expect(httpResponse.statusCode).toBe(400);
     expect(httpResponse.body).toEqual(new MissingParamException('password_confirmation'));
   });
-  
+  test('Should returns 400 if password_confirmation is invalid', () => {
+    const sut = new SignUpController();
+    const httpRequest = {
+        body: {
+            name: 'any_name',
+            email: 'any_email',
+            password: 'any_password',
+            password_confirmation: 'invalid_password'
+        }
+    };
+    const httpResponse = sut.handle(httpRequest);
+    expect(httpResponse.statusCode).toBe(400);
+    expect(httpResponse.body).toEqual(new InvalidParamException('password_confirmation'));
+  });
 });
