38e99c76336ff71ddd02c6852a75c5d2b5bea1ce
Rubem Neto <rubemcorrea0@gmail.com>
2023-09-27 10:55:15 -0300

    feat: ensure SignUpController returns 400 if no email is provided

--- src/presentation/controllers/sign-up/sign-up.ts
+++ src/presentation/controllers/sign-up/sign-up.ts
@@ -1,7 +1,15 @@
 export class SignUpController {
     handle(httpRequest: any): any{
+        const requireParams = ['name', 'email'];
+        for (const field of requireParams) {
+            if (!httpRequest.body[field]) {
+                return {
+                    statusCode: 400
+                };
+            }
+        }
         return {
-            statusCode: 400
+            statusCode: 200
         };
     }
 }
