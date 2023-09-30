9b69c648d9023ca8b44c4858e42475b0beff7df7
Rubem Neto <rubemcorrea0@gmail.com>
2023-09-30 16:14:51 -0300

    refactor: move sut to a factory method

--- src/infra/criptography/bcrypt-adapter.spec.ts
+++ src/infra/criptography/bcrypt-adapter.spec.ts
@@ -7,17 +7,24 @@ jest.mock('bcrypt', () => ({
   } 
 }));
 
+const makeSut = () => {
+  const salt:number = 12;
+  const sut = new BcryptAdapter(salt);
+  return{
+    salt, 
+    sut
+  };
+};
+
 describe('Bcrypt Adapter', () => {
   test('Should calls bcrypt with correct values', async () => {
-    const salt:number = 12;
-    const sut = new BcryptAdapter(salt);
+    const {sut, salt} = makeSut();
     const hashSpy = jest.spyOn(bcrypt, 'hash');
     await sut.encrypt('any_value');
     expect(hashSpy).toHaveBeenCalledWith('any_value', salt);
   });
   test('Should returns a hash on sucess', async () => {
-    const salt:number = 12;
-    const sut = new BcryptAdapter(salt);
+    const {sut, salt} = makeSut();
     const response = await sut.encrypt('any_value');
     expect(response).toEqual('hashed_value');
   });
