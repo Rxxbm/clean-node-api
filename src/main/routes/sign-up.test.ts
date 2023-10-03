b9faa8fa1c2d67c89f91961250e1f3cd50a3b441
Rubem Neto <rubemcorrea0@gmail.com>
2023-10-03 10:13:24 -0300

    feat: connect to database before app listen

--- src/main/routes/sign-up.test.ts
+++ src/main/routes/sign-up.test.ts
@@ -1,7 +1,24 @@
 import * as request from 'supertest';
+import * as dotenv from 'dotenv';
 import app from '../config/app';
+import { MongoHelper } from 'infra/db/helpers/mongo-helper';
 
 describe('SignUp Route', () => {
+  dotenv.config();
+
+  beforeAll(async () => {
+     await MongoHelper.connect(process.env.MONGO_URL as string);
+  })
+  
+  afterAll(async () => {
+      await MongoHelper.disconnect();
+  })
+
+  beforeEach(async () => {
+      const accountCollection = await MongoHelper.getCollection('accounts');
+      await accountCollection.deleteMany({});
+  }),
+
   test('Should return an account on success', async () => {
     await request(app).post('/api/sign-up')
     .send({
--- src/main/server.ts
+++ src/main/server.ts
@@ -1,7 +1,10 @@
 import * as dotenv from 'dotenv';
 import app from './config/app';
-
+import env from './config/env';
+import { MongoHelper } from '../infra/db/helpers/mongo-helper';
 dotenv.config();
-app.listen(5050, () => {
-    console.log('server running at http://localhost:5050');
-});
+MongoHelper.connect(env.mongoUrl).then(() => {
+    app.listen(env.port, () => {
+        console.log('server running at http://localhost:5050');
+    });
+}).catch(console.error);
